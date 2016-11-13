db101db29c968f8dc3b87b31bf8725591c7e14c9
Andrew Lee <andrew.warren.lee@gmail.com>
2016-11-12 20:43:20 -0500

    json in data.json

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import numpy as np
+import json
 
 from PIL import Image
 from pytesser.pytesser import *
 import sys
 import os, os.path
 
-def preprocess(image):
-	# image.show()
-	image = np.array(image)
-	for sublist in image:
-		for entry in sublist:
-			if sum(entry) < 750:
-				for value in entry:
-					value = 0;
-	image = Image.fromarray(image)
-	# image.show()
-	return image
-
-directory_path = '/converted-text/'
-
 image_file_name = sys.argv[1]
 im = Image.open(image_file_name)
 
-im = preprocess(im)
+# im = preprocess(im)
 text = image_to_string(im)
 text = image_file_to_string(image_file_name)
 text = image_file_to_string(image_file_name, graceful_errors=True)
 
 filename = sys.argv[1].split(".")[0]
 filename = ''.join(e for e in filename if e.isalnum() or e == '-')
-text_file_path = filename + '.txt'
+text_file_path = 'test.txt'
 
 text_file = open(text_file_path, "w+")
 text_file.write("%s" % text)
 text_file.close()
 
-print('done')
+print('done with parsing image')
+
+food_items = []
+prices = []
+
+with open('test.txt') as f:
+	for line in f:
+		line = line.strip("\n")
+		if any(char.isdigit() for char in line) and "-" in line:
+			words = line.split("-")
+			food_items.append(words[0])
+			prices.append(int(words[1]))
+
+data = dict(zip(food_items, prices))
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
